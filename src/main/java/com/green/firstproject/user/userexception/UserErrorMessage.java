610f72e9a31dbcce0d591e2a8d3adc09065c6004
Administrator <redraib05@gmail.com>
2024-06-10 16:59:23 +0900

    테스트ㅅ

---
+++ src/main/java/com/green/firstproject/user/userexception/UserErrorMessage.java
@@ -0,0 +1,9 @@
+package com.green.firstproject.user.userexception;
+
+public class UserErrorMessage {
+    public static final String USER_ID_CHECK_MESSAGE = "아이디 체크 오류";
+    public static final String USER_PASSWORD_CHECK_MESSAGE = "비밀번호 체크 오류";
+    public static final String USER_EMAIL_CHECK_MESSAGE = "이메일 체크 오류";
+
+
+}
