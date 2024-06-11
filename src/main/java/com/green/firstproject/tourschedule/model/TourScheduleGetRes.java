bdca46a3770d679b5abcc9b60bc06d9b12aa3d0d
hajju0617 <joowon023617@gmail.com>
2024-06-11 15:08:30 +0900

    Schedule Controller - PathVariable 변수 이름 지정

--- src/main/java/com/green/firstproject/tourschedule/model/TourScheduleGetRes.java
+++ src/main/java/com/green/firstproject/tourschedule/model/TourScheduleGetRes.java
@@ -1,10 +1,13 @@
 package com.green.firstproject.tourschedule.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TourScheduleGetRes {
+
     private long tourScheduleId;
+
     private String tourScheduleDay;
     private String tourScheduleStart;
     private String tourScheduleEnd;
