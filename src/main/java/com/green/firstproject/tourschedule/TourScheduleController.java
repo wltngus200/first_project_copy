585b2894d10596b441d46d080b2cc2bc1c58f6bb
hajju0617 <joowon023617@gmail.com>
2024-06-14 17:44:31 +0900

    TourSchedule Controller - resultData 값에 대한 응답코드

--- src/main/java/com/green/firstproject/tourschedule/TourScheduleController.java
+++ src/main/java/com/green/firstproject/tourschedule/TourScheduleController.java
@@ -6,6 +6,7 @@ import com.green.firstproject.tourschedule.scheduleexception.DuplicateScheduleEx
 import com.green.firstproject.tourschedule.scheduleexception.ScheduleRequiredException;
 import com.green.firstproject.tourschedule.scheduleexception.ScheduleSaveFailedException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
@@ -31,6 +32,15 @@ public class TourScheduleController {
                     "<p><strong> title</strong> : 제목(String) </p>" +
                     "<p><strong> contents</strong> : 내용(String) </p>" +
                     "<p><strong> cost</strong> : 예산(long) </p>")
+    @ApiResponse(
+            description =
+                    "<p> ResponseCode 응답 코드 </p> " +
+                            "<p>  0 : 등록 실패</p> " +
+                            "<p>  1 : 정상 </p> " +
+                            "<p> -1 : 예산에 음수를 입력하셨거나 필수값(제목, 날짜, 시각)을 입력하지 않으셨습니다 </p> " +
+                            "<p> -2 : 중복된 일정입니다</p> " +
+                            "<p> -3 : 에러 발생</p> "
+    )
     public ResultDto<Long> postSchedule(@RequestBody TourSchedulePostReq p) {
         try {
             long result = service.postSchedule(p);
@@ -45,19 +55,19 @@ public class TourScheduleController {
             return ResultDto.<Long>builder()
                     .statusCode(HttpStatus.BAD_REQUEST)
                     .resultMsg("예산에 음수를 입력하셨거나 필수값(제목, 날짜, 시각)을 입력하지 않으셨습니다")
-                    .resultData(0L)
+                    .resultData(-1L)
                     .build();
         } catch (DuplicateScheduleException e) {
             return ResultDto.<Long>builder()
                     .statusCode(HttpStatus.BAD_REQUEST)
                     .resultMsg("중복된 일정입니다")
-                    .resultData(-1L)
+                    .resultData(-2L)
                     .build();
         } catch (ScheduleSaveFailedException e) {
             return ResultDto.<Long>builder()
                     .statusCode(HttpStatus.BAD_REQUEST)
                     .resultMsg("등록 실패")
-                    .resultData(-2L)
+                    .resultData(0L)
                     .build();
         } catch (Exception e) {
             return ResultDto.<Long>builder()
@@ -73,6 +83,12 @@ public class TourScheduleController {
             "<strong > 일정 스케줄 삭제 </strong> <p></p>" +
             "<p><strong> tourId</strong> : 여행 PK (long) </p>" +
             "<p><strong> tourScheduleId</strong> : 여행 스케줄 PK (long) </p>")
+    @ApiResponse(
+            description =
+                    "<p> ResponseCode 응답 코드 </p> " +
+                            "<p>  0 : 스케줄 삭제 실패</p> " +
+                            "<p>  1 : 스케줄 삭제 성공 </p> "
+    )
     public ResultDto<Integer> deleteSchedule(@ParameterObject @ModelAttribute TourScheduleDeleteReq p) {
         try {
             int result = service.deleteSchedule(p);
@@ -150,6 +166,14 @@ public class TourScheduleController {
                     "<p><strong> contents</strong> : 내용(String) </p>" +
                     "<p><strong> cost</strong> : 예산(long) </p>" +
                     "<p><strong> tour_schedul_id</strong> : 여행 스케줄 PK </p>")
+    @ApiResponse(
+            description =
+                    "<p> ResponseCode 응답 코드 </p> " +
+                            "<p>  0 : 수정 실패</p> " +
+                            "<p>  1 : 수정 성공 </p> " +
+                            "<p> -1 : 예산에 음수를 입력하셨거나 필수값(제목, 날짜, 시각)을 입력하지 않으셨습니다 </p> " +
+                            "<p> -2 : 에러 발생</p> "
+    )
     public ResultDto<Integer> updateScheduleDay(@RequestParam(name = "tour_schedule_id") long tourScheduleId, @RequestBody TourSchedulePutReq p) {
         try {
             int result = service.updateScheduleDay(tourScheduleId, p);
