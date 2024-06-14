e86721a80c18f67f41a468bac796ce75a85bfa86
Administrator <hyeongwoojeong@gmail.com>
2024-06-14 16:55:21 +0900

    tour, tourSchedule put 로직 수정

--- src/main/java/com/green/firstproject/tour/TourControllerImpl.java
+++ src/main/java/com/green/firstproject/tour/TourControllerImpl.java
@@ -6,7 +6,6 @@ import com.green.firstproject.tour.model.*;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.annotations.Param;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
